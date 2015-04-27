471069da9dcbc6d74e2f21090108b9a1765cc8bc
coffiholic <coffiholic@users.noreply.github.com>
2015-04-27 19:59:43 +0800

    Create MyStackApp.java

---
+++ MyStackApp.java
@@ -0,0 +1,60 @@
+//U10316032 姚禹丞
+import java.util.ArrayList;
+
+public class MystackApp {
+  public static void main(String[] args) {
+    MyStack1 stack = new MyStack1();
+    stack.push("S1");
+    stack.push("S2");
+    stack.push("S");
+
+    MyStack1 stack2 = (MyStack1) (stack.clone());
+    stack2.push("S1");
+    stack2.push("S2");
+    stack2.push("S");
+
+    System.out.println(stack.getSize());
+    System.out.println(stack2.getSize());
+  }//End main methods
+}//End class
+
+class MyStack1 implements Cloneable {
+  private ArrayList<Object> list = new ArrayList<Object>();
+
+  public boolean isEmpty() {
+    return list.isEmpty();
+  }
+
+  public int getSize() {
+    return list.size();
+  }
+
+  public Object peek() {
+    return list.get(getSize() - 1);
+  }
+
+  public Object pop() {
+    Object o = list.get(getSize() - 1);
+    list.remove(getSize() - 1);
+    return o;
+  }
+
+  public void push(Object o) {
+    list.add(o);
+  }
+
+  
+  public String toString() {
+    return "stack: " + list.toString();
+  }// Override the toString in the Object class
+
+  public Object clone() {
+    try {
+      MyStack1 c = (MyStack1) super.clone();
+      c.list = (ArrayList<Object>) this.list.clone();
+      return c;
+    } catch (CloneNotSupportedException ex) {
+      return null;
+    }
+  }
+}
